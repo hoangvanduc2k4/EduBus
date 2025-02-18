f659d2c81d9ff733f2dbd335f1b38fe59f49d012
Long <LongNBHE186897@gmail.com>
2025-02-18 07:56:52 +0700

    Fix Defect ID 20

--- src/java/model/Pick.java
+++ src/java/model/Pick.java
@@ -12,9 +12,12 @@ import java.time.LocalTime;
  */
 public class Pick {
 
-    private int studentid, tripid, stopid;
-    private LocalTime picktime, droptime;
-
+    private int studentid;
+    private int tripid;
+    private int stopid;
+    private LocalTime picktime;
+    private LocalTime droptime;
+    
     public Pick() {
     }
 
